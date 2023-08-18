7930924e6c715cb12f3c6c97d5d8c1e87be2aef0
Lescuclai <lea-escudie@hotmail.fr>
2023-08-18 12:07:43 +0200

    feat: 🎸 header setup

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import homeView from './views/homeView.vue'
+</script>
 
 <template>
-  <header></header>
+  <a-page-header
+    class="header"
+    style="border: 1px solid rgb(235, 237, 240)"
+    title="Titre de la page"
+    :avatar="{ src: 'https://avatars1.githubusercontent.com/u/8186664?s=460&v=4' }"
+  />
+
+  <homeView></homeView>
 </template>
 
-<style scoped></style>
+<style scoped>
+.header {
+  position: 'sticky';
+  top: 0;
+  z-index: 1;
+  width: '100%';
+  display: 'flex';
+  background-color: aliceblue;
+}
+</style>
